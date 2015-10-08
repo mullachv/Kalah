cbc4b1e104285236311c1f1edb7384f20fe20204
mullachv <mullachv@gmail.com>
2015-10-08 00:52:46 -0400

    HW4 updated

--- ts_output_readonly_do_NOT_change_manually/src/aiService_test.js
+++ ts_output_readonly_do_NOT_change_manually/src/aiService_test.js
@@ -101,38 +101,74 @@ describe("aiService", function () {
             { set: { key: 'delta', value: { boardSideId: 1, house: 0, nitems: 1 } } }];
         expect(angular.equals(move, expectedMove)).toBe(true);
     });
-    //
-    // it("O prevents an immediate win", function() {
-    //   var move = aiService.createComputerMove(
-    //       [['X', 'X', ''],
-    //        ['O', '', ''],
-    //        ['', '', '']], 1, {maxDepth: 2});
-    //   expect(angular.equals(move[2].set.value, {row: 0, col: 2})).toBe(true);
-    // });
-    //
-    // it("O prevents another immediate win", function() {
-    //   var move = aiService.createComputerMove(
-    //       [['X', 'O', ''],
-    //        ['X', 'O', ''],
-    //        ['', 'X', '']], 1, {maxDepth: 2});
-    //   expect(angular.equals(move[2].set.value, {row: 2, col: 0})).toBe(true);
-    // });
-    //
-    // it("X finds a winning move that will lead to winning in 2 steps", function() {
-    //   var move = aiService.createComputerMove(
-    //       [['X', '', ''],
-    //        ['O', 'X', ''],
-    //        ['', '', 'O']], 0, {maxDepth: 3});
-    //   expect(angular.equals(move[2].set.value, {row: 0, col: 1})).toBe(true);
-    // });
-    //
-    // it("O finds a winning move that will lead to winning in 2 steps", function() {
-    //   var move = aiService.createComputerMove(
-    //       [['', 'X', ''],
-    //        ['X', 'X', 'O'],
-    //        ['', 'O', '']], 1, {maxDepth: 3});
-    //   expect(angular.equals(move[2].set.value, {row: 2, col: 2})).toBe(true);
-    // });
+    it("Side 0 prevents an immediate win", function () {
+        var move = aiService.createComputerMove({
+            boardSides: [
+                {
+                    store: 20,
+                    house: [1, 1, 1, 0, 0, 0],
+                    sowDir: SowDirType.RtoL
+                },
+                {
+                    store: 23,
+                    house: [0, 0, 0, 2, 0, 0],
+                    sowDir: SowDirType.LtoR
+                }
+            ]
+        }, 0, { maxDepth: 1 });
+        var expectedMove = [{ setTurn: { turnIndex: 1 } },
+            { set: { key: 'board', value: {
+                        boardSides: [
+                            {
+                                store: 20,
+                                house: [0, 2, 1, 0, 0, 0],
+                                sowDir: SowDirType.RtoL
+                            },
+                            {
+                                store: 23,
+                                house: [0, 0, 0, 2, 0, 0],
+                                sowDir: SowDirType.LtoR
+                            }
+                        ]
+                    }
+                } },
+            { set: { key: 'delta', value: { boardSideId: 0, house: 0, nitems: 1 } } }];
+        expect(angular.equals(move, expectedMove)).toBe(true);
+    });
+    it("Side 1 finds a win in 2 steps", function () {
+        var move = aiService.createComputerMove({
+            boardSides: [
+                {
+                    store: 23,
+                    house: [2, 0, 0, 1, 0, 0],
+                    sowDir: SowDirType.RtoL
+                },
+                {
+                    store: 20,
+                    house: [1, 0, 0, 0, 1, 0],
+                    sowDir: SowDirType.LtoR
+                }
+            ]
+        }, 1, { maxDepth: 3 });
+        expect(angular.equals(move[2].set.value, { boardSideId: 1, house: 4, nitems: 1 })).toBe(true);
+    });
+    it("Side 0 finds a win in 2 steps", function () {
+        var move = aiService.createComputerMove({
+            boardSides: [
+                {
+                    store: 20,
+                    house: [1, 0, 0, 0, 1, 0],
+                    sowDir: SowDirType.RtoL
+                },
+                {
+                    store: 23,
+                    house: [2, 0, 0, 1, 0, 0],
+                    sowDir: SowDirType.LtoR
+                }
+            ]
+        }, 0, { maxDepth: 3 });
+        expect(angular.equals(move[2].set.value, { boardSideId: 0, house: 4, nitems: 1 })).toBe(true);
+    });
     //
     // it("O finds a cool winning move that will lead to winning in 2 steps", function() {
     //   var move = aiService.createComputerMove(
