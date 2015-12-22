f2ef4775c70e0f3236a273c9af78668f6708e691
mullachv <mullachv@gmail.com>
2015-12-22 12:41:28 -0500

    w2h 1.93

--- src/game.ts
+++ src/game.ts
@@ -12,7 +12,7 @@ module game {
 
   export function init() {
     console.log("Translation of 'RULES_OF_KALAH' is " + translate('RULES_OF_KALAH'));
-    resizeGameAreaService.setWidthToHeight(1.67);
+    resizeGameAreaService.setWidthToHeight(1.93);
     gameService.setGame({
       minNumberOfPlayers: 2,
       maxNumberOfPlayers: 2,
